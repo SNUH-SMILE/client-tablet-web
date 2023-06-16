cafac3dc832aa5e6bd5838bd83df29585511ab0d
yb-k <kyb2206@knou.ac.kr>
2023-06-16 13:40:06 +0900

    feat:  login after move stretcher list page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -63,7 +63,7 @@ function Login({setTokenInterval}) {
                             TokenMethod.Reissue();
                         },parseInt(process.env.REACT_APP_AUTHORIZATION_REISSUE_TIME)))
                         // 메인 페이지로 이동
-                        navigate('/treatmentCenter');
+                        navigate('/stretcher/list');
                     }
                     // 비밀번호 불일치
                     else if (code === '10') {
